076d77777ef0b0d12692e9c8df1222fb087ded11
Sebastian Hartte <shartte@users.noreply.github.com>
2024-02-02 22:30:35 +0100

    Add a test case for the partial match case.

---
+++ tests/data/partial_matches/expected/TestClass.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+public class TestClass {
+    // This method tests that we consider parchment entries that are
+    // for methods with strictly fewer parameter. And we prioritize
+    // the entries with the most overlap.
+    public void m(String a, List<String> a2, int a3) {
+    }
+}
