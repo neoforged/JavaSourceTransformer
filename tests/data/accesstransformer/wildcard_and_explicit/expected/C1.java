8c145d63446777f10e30a10ee6a4fa37a752bb21
Matyrobbrt <65940752+Matyrobbrt@users.noreply.github.com>
2024-06-19 15:24:21 +0300

    Fix errors when both explicit and wildcard ATs are present (#16)
    Fixes #15

---
+++ tests/data/accesstransformer/wildcard_and_explicit/expected/C1.java
@@ -0,0 +1,14 @@
+public class C1 {
+    protected static final String S1 = "s1";
+    protected static final String S2 = "s2";
+    public static final String S3 = "s3";
+    protected static final String S4 = "s4";
+
+    public void run() {
+
+    }
+
+    public int doThing() {
+
+    }
+}
---
+++ tests/data/accesstransformer/wildcard_and_explicit/source/C1.java
@@ -0,0 +1,14 @@
+public class C1 {
+    private static final String S1 = "s1";
+    private static final String S2 = "s2";
+    private static final String S3 = "s3";
+    private static final String S4 = "s4";
+
+    private void run() {
+
+    }
+
+    private int doThing() {
+
+    }
+}
