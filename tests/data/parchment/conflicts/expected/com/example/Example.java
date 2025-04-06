49b8048db936956308d8e19589ff0ee16e2d973a
Matyrobbrt <matyrobbrt@gmail.com>
2025-04-06 15:56:21 +0300

    Catch conflicts when multiple parameters in the same scope get renamed to the same name

---
+++ tests/data/parchment/conflicts/expected/com/example/Example.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Example {
+    public void m(String in, String p_in) {
+        class Nested {
+            public void m(String p_p_in, String out) {
+
+            }
+        }
+    }
+}
---
+++ tests/data/parchment/conflicts/source/com/example/Example.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Example {
+    public void m(String a1, String ac2) {
+        class Nested {
+            public void m(String ac3, String a4) {
+
+            }
+        }
+    }
+}
